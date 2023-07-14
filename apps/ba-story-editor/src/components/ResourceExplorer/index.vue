7c908673627150160586894df801caa1ed8d46e1
notnotype <notnotype@gmail.com>
2023-07-14 15:55:35 +0800

    lint

---
+++ apps/ba-story-editor/src/components/ResourceExplorer/index.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+export default {
+  name: "ResourceExplorer",
+};
+</script>
+
+<script setup lang="ts">
+const a = ref(1);
+</script>
+
+<template>
+  <div />
+</template>
+
+<style lang="scss" scoped></style>
