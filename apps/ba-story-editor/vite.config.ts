039dab0da181585e0de102399a6a5c2bfe9402bd
Mark Chen <9006264+Mark9804@users.noreply.github.com>
2023-07-14 15:43:54 +0900

    fix: warning 导致 build cache 不生效的问题

--- apps/ba-story-editor/vite.config.ts
+++ apps/ba-story-editor/vite.config.ts
@@ -1,27 +1,26 @@
-import path from 'node:path'
-import { defineConfig } from 'vite'
-import Vue from '@vitejs/plugin-vue'
-import Pages from 'vite-plugin-pages'
-import generateSitemap from 'vite-ssg-sitemap'
-import Layouts from 'vite-plugin-vue-layouts'
-import Components from 'unplugin-vue-components/vite'
-import AutoImport from 'unplugin-auto-import/vite'
-import Markdown from 'vite-plugin-vue-markdown'
-import { VitePWA } from 'vite-plugin-pwa'
-import VueI18n from '@intlify/unplugin-vue-i18n/vite'
-import VueDevTools from 'vite-plugin-vue-devtools'
-import LinkAttributes from 'markdown-it-link-attributes'
-import Unocss from 'unocss/vite'
-import Shiki from 'markdown-it-shiki'
-
+import path from "node:path";
+import LinkAttributes from "markdown-it-link-attributes";
+import Shiki from "markdown-it-shiki";
+import { defineConfig } from "vite";
+import Pages from "vite-plugin-pages";
+import { VitePWA } from "vite-plugin-pwa";
+import VueDevTools from "vite-plugin-vue-devtools";
+import Layouts from "vite-plugin-vue-layouts";
+import Markdown from "vite-plugin-vue-markdown";
+import WebfontDownload from "vite-plugin-webfont-dl";
+import generateSitemap from "vite-ssg-sitemap";
+import VueI18n from "@intlify/unplugin-vue-i18n/vite";
+import Vue from "@vitejs/plugin-vue";
+import Unocss from "unocss/vite";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
 // @ts-expect-error failed to resolve types
-import VueMacros from 'unplugin-vue-macros/vite'
-import WebfontDownload from 'vite-plugin-webfont-dl'
+import VueMacros from "unplugin-vue-macros/vite";
 
 export default defineConfig({
   resolve: {
     alias: {
-      '~/': `${path.resolve(__dirname, 'src')}/`,
+      "~/": `${path.resolve(__dirname, "src")}/`,
     },
   },
 
@@ -36,7 +35,7 @@ export default defineConfig({
 
     // https://github.com/hannoeru/vite-plugin-pages
     Pages({
-      extensions: ['vue', 'md'],
+      extensions: ["vue", "md"],
     }),
 
     // https://github.com/JohnCampionJr/vite-plugin-vue-layouts
@@ -45,27 +44,24 @@ export default defineConfig({
     // https://github.com/antfu/unplugin-auto-import
     AutoImport({
       imports: [
-        'vue',
-        'vue-router',
-        'vue-i18n',
-        '@vueuse/head',
-        '@vueuse/core',
+        "vue",
+        "vue-router",
+        "vue-i18n",
+        "@vueuse/head",
+        "@vueuse/core",
       ],
-      dts: 'src/auto-imports.d.ts',
-      dirs: [
-        'src/composables',
-        'src/stores',
-      ],
+      dts: "src/auto-imports.d.ts",
+      dirs: ["src/composables", "src/stores"],
       vueTemplate: true,
     }),
 
     // https://github.com/antfu/unplugin-vue-components
     Components({
       // allow auto load markdown components under `./src/components/`
-      extensions: ['vue', 'md'],
+      extensions: ["vue", "md"],
       // allow auto import and register components used in markdown
       include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
-      dts: 'src/components.d.ts',
+      dts: "src/components.d.ts",
     }),
 
     // https://github.com/antfu/unocss
@@ -75,50 +71,50 @@ export default defineConfig({
     // https://github.com/antfu/vite-plugin-vue-markdown
     // Don't need this? Try vitesse-lite: https://github.com/antfu/vitesse-lite
     Markdown({
-      wrapperClasses: 'prose prose-sm m-auto text-left',
+      wrapperClasses: "prose prose-sm m-auto text-left",
       headEnabled: true,
       markdownItSetup(md) {
         // https://prismjs.com/
         md.use(Shiki, {
           theme: {
-            light: 'vitesse-light',
-            dark: 'vitesse-dark',
+            light: "vitesse-light",
+            dark: "vitesse-dark",
           },
-        })
+        });
         md.use(LinkAttributes, {
           matcher: (link: string) => /^https?:\/\//.test(link),
           attrs: {
-            target: '_blank',
-            rel: 'noopener',
+            target: "_blank",
+            rel: "noopener",
           },
-        })
+        });
       },
     }),
 
     // https://github.com/antfu/vite-plugin-pwa
     VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.svg', 'safari-pinned-tab.svg'],
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.svg", "safari-pinned-tab.svg"],
       manifest: {
-        name: 'Vitesse',
-        short_name: 'Vitesse',
-        theme_color: '#ffffff',
+        name: "Vitesse",
+        short_name: "Vitesse",
+        theme_color: "#ffffff",
         icons: [
           {
-            src: '/pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
+            src: "/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
           },
           {
-            src: '/pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
           },
           {
-            src: '/pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable',
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
           },
         ],
       },
@@ -129,7 +125,7 @@ export default defineConfig({
       runtimeOnly: true,
       compositionOnly: true,
       fullInstall: true,
-      include: [path.resolve(__dirname, 'locales/**')],
+      include: [path.resolve(__dirname, "locales/**")],
     }),
 
     // https://github.com/feat-agency/vite-plugin-webfont-dl
@@ -141,27 +137,31 @@ export default defineConfig({
 
   // https://github.com/vitest-dev/vitest
   test: {
-    include: ['test/**/*.test.ts'],
-    environment: 'jsdom',
+    include: ["test/**/*.test.ts"],
+    environment: "jsdom",
     deps: {
-      inline: ['@vue', '@vueuse', 'vue-demi'],
+      inline: ["@vue", "@vueuse", "vue-demi"],
     },
   },
 
   // https://github.com/antfu/vite-ssg
   ssgOptions: {
-    script: 'async',
-    formatting: 'minify',
+    script: "async",
+    formatting: "minify",
     crittersOptions: {
       reduceInlineStyles: false,
     },
     onFinished() {
-      generateSitemap()
+      generateSitemap();
     },
   },
 
   ssr: {
     // TODO: workaround until they support native ESM
-    noExternal: ['workbox-window', /vue-i18n/],
+    noExternal: ["workbox-window", /vue-i18n/],
   },
-})
+
+  build: {
+    chunkSizeWarningLimit: 1500,
+  },
+});
