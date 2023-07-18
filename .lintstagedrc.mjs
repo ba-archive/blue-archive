2aea17e34eb3a44d2c216002249473fe9d82f9eb
notnotype <notnotype@gmail.com>
2023-07-18 11:43:20 +0800

    rush: disable lint-staged fix

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -2,6 +2,6 @@ export default {
   '{apps,lib}/**/*.{js,jsx,ts,tsx,vue}': (files) => {
     // from `files` filter those _NOT_ matching `*test.js`
     // const match = micromatch.not(files, 'apps/ba-story-editor/src/components.d.ts')
-    return `eslint ${files.join(' ')} --fix --cached`
+    return `eslint ${files.join(' ')} --cached`
   } 
 }
