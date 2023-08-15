9beb67665a065832d353311d0211412413809438
Mark Chen <9006264+Mark9804@users.noreply.github.com>
2023-08-15 14:04:14 +0900

    style(widget): 删除废弃样式

--- apps/blue-archive-story-viewer/src/components/widgets/ThemeSwitcher.vue
+++ apps/blue-archive-story-viewer/src/components/widgets/ThemeSwitcher.vue
@@ -83,40 +83,4 @@ onUnmounted(() => {
     grid-area: light-mode-icon;
   }
 }
-
-input.switch {
-  display: grid;
-  grid-template-columns: 1fr;
-  grid-template-areas: "track";
-  grid-area: switch;
-  align-items: center;
-  justify-items: start;
-  appearance: none;
-  -webkit-appearance: none;
-  transition: all 0.375s ease-in-out;
-  cursor: pointer;
-  box-sizing: content-box;
-  outline: none !important;
-  box-shadow: var(--style-switch-track-shadow);
-  border: none !important;
-  background-color: var(--color-switch-track);
-  min-width: 3rem;
-  max-width: 3rem;
-
-  /* button-thumb */
-  &::after {
-    grid-area: track;
-    justify-self: start;
-    box-shadow: var(--style-switch-shadow);
-    border-radius: 50%;
-    background: var(--style-switch-texture);
-    width: 1.25rem;
-    height: 1.25rem;
-    content: "";
-  }
-
-  &:checked::after {
-    justify-self: end;
-  }
-}
 </style>
