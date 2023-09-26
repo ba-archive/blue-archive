63b9931dabc2c8bb5fa621a978b7f3e211f999cf
diyigemt <qwe1355247243@gmail.com>
2023-09-26 09:23:00 +0800

    fix(player): fix #171

--- lib/ba-story-player/lib/layers/textLayer/BaDialog.vue
+++ lib/ba-story-player/lib/layers/textLayer/BaDialog.vue
@@ -1262,7 +1262,9 @@ $text-outline: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;
   position: absolute;
   top: 0;
   left: 0;
+  border-radius: 6px;
   width: 100%;
   height: 100%;
+  overflow: hidden;
 }
 </style>
