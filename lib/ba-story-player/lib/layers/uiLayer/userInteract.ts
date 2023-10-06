ad85aaab9ca44de95fe007ad62663bf2c07166d9
Camille <camille_0315@buaa.edu.cn>
2023-10-06 15:23:58 +0800

    feat(player): 绑定滚轮下滑为播放下一句, close #127 (#182)
    * feat(player): 绑定滚轮下滑为播放下一句, close #127
    
    * fix(player): 滚轮下滑节流

--- lib/ba-story-player/lib/layers/uiLayer/userInteract.ts
+++ lib/ba-story-player/lib/layers/uiLayer/userInteract.ts
@@ -56,6 +56,7 @@ const keyUpEvent = (e: KeyboardEvent) => {
   }
 };
 
+let wheelTimer: number | undefined;
 const wheelEvent = (e: WheelEvent & { [key: string]: any }) => {
   const delta = e.wheelDelta ? e.wheelDelta : -e.detail;
   if (eventEmitter.isStoryLogShow || !isPlayerFocus()) {
@@ -63,6 +64,13 @@ const wheelEvent = (e: WheelEvent & { [key: string]: any }) => {
   }
   if (delta >= 0) {
     eventBus.emit("showStoryLog", true);
+  } else {
+    if (!wheelTimer) {
+      wheelTimer = setTimeout(() => {
+        interactNext();
+        wheelTimer = undefined;
+      }, 200);
+    }
   }
 };
 
