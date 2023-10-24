215dda9ec34c4c81ad73f06b66df0659a622075d
Mark Chen <9006264+Mark9804@users.noreply.github.com>
2023-10-24 23:48:18 +0900

    Update index.ts

--- lib/ba-story-player/lib/index.ts
+++ lib/ba-story-player/lib/index.ts
@@ -800,7 +800,7 @@ export const resourcesLoader = {
     );
     excelPromiseArray.push(
       axios
-        .get(utils.getResourcesUrl("excel", "BGMExcelTable_dev.json")) // FIXME: 临时使用dev版
+        .get(utils.getResourcesUrl("excel", "BGMExcelTable.json"))
         .then(res => {
           for (const i of res.data["DataList"]) {
             privateState.BGMExcelTable.set(i["Id"], i);
