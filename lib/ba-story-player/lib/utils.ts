0049b6a8684c24c27fc59ab623fd9036da343b70
ourandream <ouranhuang@outlook.com>
2023-11-10 14:43:41 +0800

    feat: 完成zIndex utils

--- lib/ba-story-player/lib/utils.ts
+++ lib/ba-story-player/lib/utils.ts
@@ -8,10 +8,18 @@ export function waitMs(ms: number) {
 }
 
 /**
- * pixi内各层zindex的基础数字, 各层的zindex范围为基数-基数+50
+ * 各层zindex的基础数字, 各层的zindex范围为基数-基数+50
  */
 export const ZINDEXBASE = {
   bg: 0,
+  /**
+   *bgEffect可根据实际情况使用其他层的index
+   */
+  effect: 100,
+  character: 20,
+  l2d: 300,
+  show: 400,
+  ui: 500,
 };
 
 export function timelineToPauseAble(tl: ReturnType<typeof gsap.timeline>) {
