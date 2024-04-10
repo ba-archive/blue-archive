09bb77d0cc593334b53a6b0a157b1b4e8cb1bf14
Mark Chen <9006264+Mark9804@users.noreply.github.com>
2024-04-11 03:30:47 +0900

    模型换成 opus

--- apps/ba-online-toolbox/src/tools/public/helper/AnthropicTranslationService.ts
+++ apps/ba-online-toolbox/src/tools/public/helper/AnthropicTranslationService.ts
@@ -38,8 +38,8 @@ const instance = axios.create({
 });
 
 const rag_request = {
-  model: "claude-3-haiku-20240307",
-  // model: "claude-3-opus-20240229",
+  // model: "claude-3-haiku-20240307",
+  model: "claude-3-opus-20240229",
   max_tokens: 1000,
   temperature: 0,
   system:
