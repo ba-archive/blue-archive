84a25782214781ba4aa4aae4894108715ae12327
Mark Chen <9006264+Mark9804@users.noreply.github.com>
2024-06-24 17:09:33 +0900

    format(toolbox): format

--- apps/ba-online-toolbox/src/tools/ScenarioEditor/components/OriginalTextDisp.vue
+++ apps/ba-online-toolbox/src/tools/ScenarioEditor/components/OriginalTextDisp.vue
@@ -97,7 +97,11 @@ const html = computed(() => {
   <div
     class="p-4 pt-2 pb-2 rounded-sm border-solid border-1 border-[var(--color-arona-blue)] overflow-y-scroll select-auto max-w-[664px]"
   >
-    <div class="select-text w-full" v-if="!props.preferSemantic || !searchSemanticDict()" v-html="html" />
+    <div
+      class="select-text w-full"
+      v-if="!props.preferSemantic || !searchSemanticDict()"
+      v-html="html"
+    />
     <div v-else class="flex flex-col gap-1 w-full flex-wrap">
       <div v-for="semanticLine in semantics" class="flex flex-wrap">
         <semantic-unit
