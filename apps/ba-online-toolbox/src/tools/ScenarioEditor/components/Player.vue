532a53ecee15c0d143a074702ef2fb4f5fcf5350
Mark Chen <9006264+Mark9804@users.noreply.github.com>
2024-08-04 22:18:27 +0900

    fix(toolbox): 初始化后剧情节点错误跳转

--- apps/ba-online-toolbox/src/tools/ScenarioEditor/components/Player.vue
+++ apps/ba-online-toolbox/src/tools/ScenarioEditor/components/Player.vue
@@ -4,7 +4,6 @@
       <ba-story-player
         class="player-body rounded-small"
         v-if="showPlayer"
-        @initiated="handlePlayerInit"
         :story="mainStore.getScenario"
         :language="playerLanguage"
         :width="playerContainerWidth - 24"
@@ -83,14 +82,6 @@ watchDebounced(
     debounce: 200,
   }
 );
-
-function handlePlayerInit() {
-  StoryPlayer.value &&
-    StoryPlayer.value.hotReplaceStoryUnit(
-      mainStore.getScenario.content[config.getSelectLine],
-      config.getSelectLine
-    );
-}
 
 const isPreviewMode = computed(() => config.getPreviewMode);
 const targetLanguage = computed(() => config.getTargetLang);
