6e551d0d85ee3da59a9e29a1ec9cbe4a14b03a78
Mark Chen <9006264+Mark9804@users.noreply.github.com>
2024-09-03 18:15:32 +0900

    feat(viewer-next/eden/input): 聚焦动画

--- apps/viewer-next/packages/eden-design/components/input/EInput.vue
+++ apps/viewer-next/packages/eden-design/components/input/EInput.vue
@@ -53,32 +53,32 @@ const slots = useSlots();
 </script>
 
 <template>
-    <span
-      class="eden-ui eden-ui__input eden-ui__input--wrapper rounded flex gap-1 overflow-clip"
-      :class="inputClass"
-      :style="inputStyle"
-    >
-      <span class="eden-ui eden-ui__input--prefix flex" v-if="slots.prefix">
-        <slot name="prefix" />
-      </span>
-      <input
-        :type="type"
-        class="eden-ui eden-ui__input--input flex flex-1 items-end"
-        :style="{
-          textAlign: align,
-        }"
-        v-model="model"
-        @input="handleInput"
-        :placeholder="placeholder"
-        :disabled="disabled"
-        :min="type === 'number' ? min : undefined"
-        :max="type === 'number' ? max : undefined"
-        :step="type === 'number' ? step : undefined"
-      />
-      <span class="eden-ui eden-ui__input--suffix flex" v-if="slots.suffix">
-        <slot name="suffix" />
-      </span>
+  <span
+    class="eden-ui eden-ui__input eden-ui__input--wrapper rounded flex gap-1 overflow-clip"
+    :class="inputClass"
+    :style="inputStyle"
+  >
+    <span class="eden-ui eden-ui__input--prefix flex" v-if="slots.prefix">
+      <slot name="prefix" />
     </span>
+    <input
+      :type="type"
+      class="eden-ui eden-ui__input--input flex flex-1 items-end"
+      :style="{
+        textAlign: align,
+      }"
+      v-model="model"
+      @input="handleInput"
+      :placeholder="placeholder"
+      :disabled="disabled"
+      :min="type === 'number' ? min : undefined"
+      :max="type === 'number' ? max : undefined"
+      :step="type === 'number' ? step : undefined"
+    />
+    <span class="eden-ui eden-ui__input--suffix flex" v-if="slots.suffix">
+      <slot name="suffix" />
+    </span>
+  </span>
 </template>
 
 <style scoped lang="scss">
@@ -86,20 +86,22 @@ const slots = useSlots();
 
 .eden-ui__input {
   $border-width: 1px;
+
   &--wrapper {
+    will-change: background-color, box-shadow;
     border-bottom: none;
     padding-left: 10px;
     padding-right: 10px;
     font-size: 14px;
     line-height: 22px;
+    box-shadow: inset 0 0 0 1px transparent;
     filter: blur(0); // stacking context
 
-    box-shadow: inset 0 0 0 1px transparent;
-
+    &::before,
     &::after {
       content: "";
       position: absolute;
-      left: -$border-width;
+      left: 0;
       bottom: 0px;
       width: 100%;
       height: $border-width;
@@ -107,19 +109,25 @@ const slots = useSlots();
       transition: all 0.3s ease-in-out;
     }
 
+    &::after {
+      will-change: transform;
+      background: $arona-blue-6;
+      transform: rotate3d(0, 1, 0, 90deg);
+    }
+
     &:hover,
     &:focus,
     &:focus-within {
       box-shadow: inset 0 0 0 1px $border-3;
-
-      &::after {
-        background: $arona-blue-6;
-      }
     }
 
     &:focus,
     &:focus-within {
       background: $fill-base;
+
+      &::after {
+        transform: rotate3d(0, 1, 0, 0deg);
+      }
     }
   }
 
