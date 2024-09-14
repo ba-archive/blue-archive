849c527165371d12f96b8be7f5cfc766a76ac99f
Mark Chen <9006264+Mark9804@users.noreply.github.com>
2024-09-14 13:51:30 +0900

    fix(viewer-next/eden/icon): make safari happy

--- apps/viewer-next/packages/eden-design/components/icon/EIconAdd.vue
+++ apps/viewer-next/packages/eden-design/components/icon/EIconAdd.vue
@@ -5,7 +5,9 @@
     fill="none"
     viewBox="0 0 24 24"
     xmlns="http://www.w3.org/2000/svg"
-    :transform="`rotate(${rotate})`"
+    :style="{
+      transform: `rotate(${rotate}deg)`,
+    }"
   >
     <path
       d="M11.883 3.007 12 3a1 1 0 0 1 .993.883L13 4v7h7a1 1 0 0 1 .993.883L21 12a1 1 0 0 1-.883.993L20 13h-7v7a1 1 0 0 1-.883.993L12 21a1 1 0 0 1-.993-.883L11 20v-7H4a1 1 0 0 1-.993-.883L3 12a1 1 0 0 1 .883-.993L4 11h7V4a1 1 0 0 1 .883-.993L12 3l-.117.007Z"
--- apps/viewer-next/packages/eden-design/components/icon/EIconSubtract.vue
+++ apps/viewer-next/packages/eden-design/components/icon/EIconSubtract.vue
@@ -5,7 +5,9 @@
     fill="none"
     viewBox="0 0 24 24"
     xmlns="http://www.w3.org/2000/svg"
-    :transform="`rotate(${rotate})`"
+    :style="{
+      transform: `rotate(${rotate}deg)`,
+    }"
   >
     <path
       d="M3.997 13H20a1 1 0 1 0 0-2H3.997a1 1 0 1 0 0 2Z"
