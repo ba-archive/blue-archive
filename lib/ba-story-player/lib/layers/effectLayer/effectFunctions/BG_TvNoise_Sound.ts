5e393a09443edcb5f5d0b6bc7451f2039affc9cb
Camille <camille_0315@buaa.edu.cn>
2024-10-18 01:20:47 +0800

    fix(player): 解决电视机雪花特效开局爆闪的问题 (#261)
    * fix(player): 解决电视机雪花特效开局爆闪的问题
    
    * chore(player): 调整 TvNoise 视觉表现
    
    ---------
    
    Co-authored-by: mark9804 <9006264+mark9804@users.noreply.github.com>

--- lib/ba-story-player/lib/layers/effectLayer/effectFunctions/BG_TvNoise_Sound.ts
+++ lib/ba-story-player/lib/layers/effectLayer/effectFunctions/BG_TvNoise_Sound.ts
@@ -2,8 +2,11 @@ import eventBus from "@/eventBus";
 import { usePlayerStore } from "@/stores";
 import { Container, Sprite, Texture, Graphics, filters } from "pixi.js";
 import { BGEffectHandlerFunction } from "@/types/effectLayer";
+import { OldFilmFilter } from "@pixi/filter-old-film";
 
-const BG_TvNoise_Sound: BGEffectHandlerFunction<'BG_TvNoise_Sound'> = async () => {
+const BG_TvNoise_Sound: BGEffectHandlerFunction<
+  "BG_TvNoise_Sound"
+> = async () => {
   // 原理是使用噪点滤镜产生电视机雪花的效果
   const { app } = usePlayerStore();
   const { width: appWidth, height: appHeight } = app.view;
@@ -11,46 +14,52 @@ const BG_TvNoise_Sound: BGEffectHandlerFunction<'BG_TvNoise_Sound'> = async () =
   const container = new Container();
   app.stage.addChild(container);
 
-  // 第一层，电视雪花效果
-  const screen = new Sprite(Texture.WHITE);
-  screen.tint = 0x000000; // 白色开局会爆闪一下
-  screen.width = appWidth;
-  screen.height = appHeight;
-  container.addChild(screen);
+  // 第一层，背景
+  const bg = new Sprite(Texture.WHITE);
+  bg.tint = 0x505050;
+  bg.width = appWidth;
+  bg.height = appHeight;
+  container.addChild(bg);
 
-  // 第二层，渐变效果
-  const mask = new Graphics();
-  mask.beginFill(0x000000);
-  mask.drawRect(0, 0, appWidth, appHeight / 4);
-  mask.drawRect(0, 0, appWidth / 4, appHeight);
-  mask.drawRect(0, appHeight * 3 / 4, appWidth, appHeight / 4);
-  mask.drawRect(appWidth * 3 / 4, 0, appWidth / 4, appHeight);
-  mask.endFill();
-  mask.filters = [new filters.AlphaFilter(0.3), new filters.BlurFilter(50)];
+  // 第二层，雪花 + 暗角遮罩
+  const mask = new Sprite(Texture.WHITE);
+  mask.width = appWidth;
+  mask.height = appHeight;
+  mask.alpha = 0.8;
+  mask.filters = [
+    new filters.NoiseFilter(8),
+    new filters.BlurFilter(0.5),
+    new OldFilmFilter({
+      noise: 0.47, // 不知道为什么不生效，可能 pixi 6 兼容问题？
+      sepia: 0,
+      noiseSize: 1.1,
+      scratch: 0, // 也是不生效，但是为防诈尸先禁用
+      vignetting: 0.2,
+      vignettingAlpha: 0.9,
+      vignettingBlur: 0.8,
+    }),
+  ];
+
   container.addChild(mask);
 
-  // 第三层，纵向移动的黑色条纹
+  // 第二层，纵向移动的黑色条纹
   const bar = new Graphics();
   bar.beginFill(0x000000);
   bar.drawRect(0, -appHeight / 2, appWidth, appHeight / 6);
-  bar.drawRect(0, -appHeight * 3 / 2, appWidth, appHeight / 12);
-  bar.drawRect(0, -appHeight * 5 / 2, appWidth, appHeight / 3);
+  bar.drawRect(0, (-appHeight * 3) / 2, appWidth, appHeight / 12);
+  bar.drawRect(0, (-appHeight * 5) / 2, appWidth, appHeight / 3);
   bar.endFill();
   bar.filters = [new filters.AlphaFilter(0.3), new filters.BlurFilter(5)];
   container.addChild(bar);
 
   let count = 0;
-  const tvNoiseSpeed = 5;
+  const tvNoiseSpeed = 3;
   const barMoveSpeed = 20;
   const animation = (delta: number) => {
     // 每隔一段时间添加噪点滤镜
     count += delta;
-    if (count > tvNoiseSpeed) {
-      screen.filters = [
-        new filters.NoiseFilter(5),
-        new filters.BlurFilter(0.5),
-        new filters.FXAAFilter(),
-      ];
+    if (count > tvNoiseSpeed && !!mask.filters) {
+      (mask.filters[0] as any).seed = Math.random(); // 仅更新噪点滤镜
       count %= tvNoiseSpeed;
     }
     // 黑色条纹纵向移动
@@ -58,7 +67,7 @@ const BG_TvNoise_Sound: BGEffectHandlerFunction<'BG_TvNoise_Sound'> = async () =
     if (bar.y > appHeight * 3.5) {
       bar.y = 0;
     }
-  }
+  };
   app.ticker.add(animation);
 
   eventBus.emit("playBgEffectSound", "BG_Flash_Sound");
@@ -66,7 +75,7 @@ const BG_TvNoise_Sound: BGEffectHandlerFunction<'BG_TvNoise_Sound'> = async () =
   return async () => {
     app.ticker.remove(animation);
     container.destroy();
-  }
-}
+  };
+};
 
 export default BG_TvNoise_Sound;
