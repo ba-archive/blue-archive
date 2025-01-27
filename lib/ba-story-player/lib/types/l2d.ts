1cf53f6ad7951a4a40d3ab3a54a2045f4b8f6db4
mark9804 <9006264+mark9804@users.noreply.github.com>
2025-01-27 22:02:40 +0800

    chore(player): 补一点类型注释

--- lib/ba-story-player/lib/types/l2d.ts
+++ lib/ba-story-player/lib/types/l2d.ts
@@ -34,11 +34,16 @@ export type IL2dConfig = {
   };
 };
 export type IL2dPlayQue = {
+  /** 动画名称 */
   name: string;
   animation: string;
+  /** 动画播放延迟 */
   pushDelay?: number;
+  /** 第一次淡入转场时机 */
   fadeTime?: number;
+  /** 第二次淡入转场时机 */
   secondFadeTime?: number;
+  /** 自定义淡入转场 */
   customFade?: Array<{
     customFadeTime: number;
     customFadeColor?: string;
@@ -46,15 +51,19 @@ export type IL2dPlayQue = {
     solidStateDuration?: number;
     toNormalDuration?: number;
   }>;
+  /** 声音 */
   sounds?: {
+    /** 声音路径 */
     fileName: string;
+    /** 声音播放时间 */
     time: number;
     /**
      * 声音大小, 默认为2
      */
     volume?: number;
   }[];
-  /** 和后一个动画是否fade */
+  /** 是否淡入淡出过渡到下一动画 */
   fade?: boolean;
+  /** 自定义转场 */
   customTransitions?: TCustomTransition[];
 };
