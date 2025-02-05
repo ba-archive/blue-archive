a4a8b3383b6eb631e11ad855588ff1dbfdfba30c
mark9804 <9006264+mark9804@users.noreply.github.com>
2025-02-05 12:16:00 +0800

    style(toolbox): 更新mmt编辑器气泡样式

--- apps/ba-online-toolbox/src/tools/v2-MomotalkEditor/components/MmtkComponents/MmtkUnit.vue
+++ apps/ba-online-toolbox/src/tools/v2-MomotalkEditor/components/MmtkComponents/MmtkUnit.vue
@@ -17,7 +17,7 @@ const to = computed(
       useMomotalkEditorStore.getTargetTranslation.toUpperCase()) as keyof Content
 );
 
-const isImage = computed(() => props.msg.ImagePath)
+const isImage = computed(() => props.msg.ImagePath);
 </script>
 
 <template>
@@ -28,23 +28,30 @@ const isImage = computed(() => props.msg.ImagePath)
     }"
   >
     <div
-      px-4
-      py-2
       z-0
       class="talk-bubble max-w-80 relative"
       :class="{
         'talk-bubble--answer': 'Answer' === msg.MessageCondition,
         'talk-bubble--favor': msg.FavorScheduleId != 0,
+        'px-4 py-2': !isImage,
       }"
     >
-      <div class="orininal-msg pl-[2px]">
+      <div
+        class="orininal-msg"
+        :class="{
+          'pl-[2px]': !isImage,
+          'lh-0': isImage
+        }"
+      >
         <span v-if="!isImage">{{ props.msg[from] }}</span>
         <img
           w-40
           v-else
+          m-0
           :src="
             'https://yuuka.cdn.diyigemt.com/image/ba-all-data/' +
-            props.msg.ImagePath + '.png'
+            props.msg.ImagePath +
+            '.png'
           "
         />
       </div>
@@ -57,7 +64,6 @@ const isImage = computed(() => props.msg.ImagePath)
           placeholder="输入内容翻译"
           width="auto"
         />
-        <!-- <a-input v-model="msg[to] as string" ></a-input> -->
       </div>
     </div>
   </div>
@@ -67,12 +73,29 @@ const isImage = computed(() => props.msg.ImagePath)
 .talk-bubble {
   @apply rounded-md bg-white @dark:bg-slate-700 @dark:text-gray-2;
 
+  &::after {
+    content: "";
+    position: absolute;
+    top: calc(50% - 5px);
+    left: -10px;
+    border-color: transparent;
+    @apply w-0 h-0 border-solid border-5 border-t-white @dark:border-t-slate-700 rotate-[90deg];
+  }
+
   &--answer {
     @apply bg-[#4a91e7] text-white @dark:bg-[#397ee1];
+
+    &::after {
+      @apply left-[100%] rotate-[-90deg] border-t-[#4a91e7] @dark:border-t-[#397ee1];
+    }
   }
 
   &--favor {
     @apply bg-[#E62C8C]! text-white @dark:bg-[#eb509b]!;
+
+    &::after {
+      @apply border-t-[#E62C8C] @dark:border-t-[#eb509b];
+    }
   }
 }
 </style>
